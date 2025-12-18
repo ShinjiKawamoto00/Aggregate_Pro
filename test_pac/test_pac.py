e6c342afd30bc26fdab296058d379f5465ac2709
unknown <shinkawa66@gmail.com>
2025-12-18 11:22:38 +0900

    Update

--- test_pac/test_pac.py
+++ test_pac/test_pac.py
@@ -1,12 +1,23 @@
+import os
 import ssl
 import urllib3
 from pypac import PACSession
 
-# グローバルSSL検証無効
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib3.disable_warnings()
+# 環境変数 全無効化（最上部）
+os.environ['PYTHONHTTPSVERIFY'] = '0'
+os.environ['REQUESTS_CA_BUNDLE'] = ''
+os.environ['CURL_CA_BUNDLE'] = ''
+os.environ['SSL_CERT_FILE'] = ''
 
-session = PACSession()
+# グローバルSSL無効
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+session = PACSession(
+    allow_non_idn=True,
+    socket_timeout=30
+)
+
 resp = session.get('https://payment.neoluxinc.com/')
 print(f"resp.status_code:{resp.status_code}")
 print(f"resp.text[:100]:{resp.text[:100]}")
